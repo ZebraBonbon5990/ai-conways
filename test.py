a6a7c72a08ba2e2f37a95fe7ba45b72bbc6791d2
ZebraBonbon5990 <elin.rohner6@gmail.com>
2024-08-12 09:46:20 +0200

    added simple rendering

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import pygame as pg
 from matplotlib import pyplot as plt
-import time
-import sys
 
 living_cells = [[0,0], [1,0], [2,0], [1,1], [1,2], [2,-1], [3,-1], [4, 0], [5,0], [5,1], [5,2], [4, 3], [3, 2], [2,2]] #list containing coordinates of living cells in the form [x, y]
 
